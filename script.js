4ac10411ad31f087d723be3d18df08ef76f38699
Luccas Figueira <luccasfigueiragc@gmail.com>
2025-03-27 09:43:03 -0300

    javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("Seja muito, Bem vindo!");
+var numero = prompt("insira o ano que nasceu: ");
+var letra = prompt("insira seu nome: ");
+alert("certo!");
+var idade = (2025 - numero);
+alert(`OK! ${letra} você possui ${idade} anos de idade!`);
+console.table({letra, numero});
+
+
